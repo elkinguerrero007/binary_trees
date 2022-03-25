bca30450b9034369310abffe487458ca2d7a7187
elkinguerrero007 <3439@holbertonschool.com>
2022-03-25 03:39:33 +0000

    binary_tree

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -9,14 +9,11 @@
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-  binary_tree_t *parent;
-
-  if (!node || !node->parent)
-    return (NULL);
-
-  parent = node->parent;
-  if (node == parent->left)
-    return (parent->right);
-
-  return (parent->left);
+binary_tree_t *parent;
+if (!node || !node->parent)
+return (NULL);
+parent = node->parent;
+if (node == parent->left)
+return (parent->right);
+return (parent->left);
 }
